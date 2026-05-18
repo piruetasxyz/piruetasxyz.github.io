bc983107f22edb1ed9ed8a5b87fc9b668d12c966
janisepulveda <janis.sepulveda@mail.udp.cl>
2026-05-18 16:56:33 -0400

    agregar yamls

--- nav.js
+++ nav.js
@@ -12,29 +12,29 @@ const navbarContent = `
     </h1>
     
     <h1><a href="/info.html">info</a></h1>
-    <h1><a href="/people/index.html"><span class="es">personas</span><span class="en">people</span></a></h1>
+    <h1><a href="/personas/index.html"><span class="es">personas</span><span class="en">people</span></a></h1>
     
     <hr style="border: 0; border-top: 1px solid black; margin: 20px 0;">
     
     <h3 class="es">popusintes</h3><h3 class="en">popusynths</h3>
     <ol>
-        <li><a href="/projects/parla/index.html">parla</a> (2025)</li>
-        <li><a href="/projects/osca/index.html">osca</a> (soon)</li>
+        <li><a href="/proyectos/parla/index.html">parla</a> (2025)</li>
+        <li><a href="/proyectos/osca/index.html">osca</a> (soon)</li>
     </ol>
 
     <h3>software</h3>
     <ol>
-        <li><a href="/projects/redondela/index.html">redondela</a></li>
+        <li><a href="/proyectos/redondela/index.html">redondela</a></li>
     </ol>
 
     <h3>hardware</h3>
     <ol>
-        <li><a href="/projects/gerassic-organ/index.html">gerassic organ</a></li>
+        <li><a href="/proyectos/gerassic-organ/index.html">gerassic organ</a></li>
     </ol>
 
     <h3 class="es">enseñanza</h3><h3 class="en">teaching</h3>
     <ol>
-        <li><a href="/projects/talleres-momentos/index.html">talleres momentos</a> (2023)</li>
+        <li><a href="/proyectos/talleres-momentos/index.html">talleres momentos</a> (2023)</li>
     </ol>
 `;
 
